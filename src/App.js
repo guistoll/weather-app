7e3971a5cbb7c43a055b1872bc0f7069369597d0
Guilherme Stollmeier <gui@twel.io>
2019-04-06 23:29:36 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import WeekWeather from './WeekWeather';
+import Autocomplete from './components/Autocomplete'
+import Weather from './components/Weather';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+    }
+  }
 
   render() {
     return (
-      <div>
-        <WeekWeather />
+      <div css={{ fontFamily: 'Lato' }}>
+        <Autocomplete getCity={(selectedCity) => this.setState({ city: selectedCity })} />
+        <ShowWeather city={this.state.city} />
       </div>
     )
   }
+}
+
+function ShowWeather(props) {
+  const city = props.city;
+
+  if (city) {
+    return <Weather key={city} location={city} />
+  }
+
+  return '';
 }
