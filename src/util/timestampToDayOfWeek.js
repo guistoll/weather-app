90502b2012c12d580fab99ed98cdabbcf284df6c
Guilherme Stollmeier <gui@twel.io>
2019-04-06 23:31:31 -0300

    Utils timestamp to day of week

--- src/util/timestampToDayOfWeek.js
+++ src/util/timestampToDayOfWeek.js
@@ -1,6 +1,10 @@
 // converts UNIX timestamp to day of week
 export default function timestampToDayOfWeek(timestamp, type) {
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  const days = type === 'short' ?
+    ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    :
+    ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   const date = new Date();
   date.setTime(timestamp * 1000);
